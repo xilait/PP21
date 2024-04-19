bca45c8602c39edb10bea10a67037e823249e7c3
khabi <khabi@ASUSG15>
2024-04-19 08:57:05 +0300

    Добавьте файлы проекта.

---
+++ PP21/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a3 = 1;
+            int b = 1;
+            Console.WriteLine("Введи выбор");
+            int c = int.Parse(Console.ReadLine());
+            switch (c)
+            {
+                case 1:
+                    a3 = int.Parse(Console.ReadLine());
+                    break;
+                case 2:
+                    b = int.Parse(Console.ReadLine());
+                    break;
+                case 3:
+                    Console.WriteLine(a3 + b);
+                    break;
+                case 4:
+                    Console.WriteLine(a3 - b);
+                    break;
+                case 5:
+                    Console.WriteLine(a3 * b);
+                    break;
+                case 6:
+                    Console.WriteLine(a3 / b);
+                    break;
+            }
+        }
+    }
+}
